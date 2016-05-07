bc44d08adc4520b890298c82a082d82d749a1ce5
ceholmes <chuck.holmes@gmail.com>
2016-05-07 11:44:20 -0400

    added midi builders

--- spec/midi.builder.spec.js
+++ spec/midi.builder.spec.js
@@ -1,37 +1,45 @@
 describe('Midi.Builder', function () {
-     
+
     var _b64 = "TVRoZAAAAAYAAQABAPBNVHJrAAAAlQD/ARBQb3dlcmVkIGJ5IE1pZGlvAP8CCUpTRmFuYXRpYwD/Aw1Mb3ZlIFdyYW5nbGVyAP8ECEtleWJvYXJkAP8FEE9oaCBiYWJ5IHllYWguLi4A/wYFSW50cm8A/wcHRGFuY2VycwD/UQMGGoAA/1gEBgMYCACQPFqBAIA8WgCwB4AAoD5AAMAkANB/AP8gAQoA/y8A";    
     var _binary = window.atob(_b64);   
     var _buffer = Buffer.utils.byteStringToBuffer(_binary);  
-   
-    it('should build Detail', function () {        
-        
+
+    it('should build Detail', function () {
+
+        var builder = new Midi.Builder();
         var reader = new Midi.Reader();
         var input = reader.read(_buffer);
         var track = input.tracks[0];
-        
-        var builder = new Midi.Builder();
-        
+
         var details = [];
-               
-        //console.log ('details ---');
+
         track.forEach(function (message) {
             var detail = builder.buildDetail(message);
             details.push(detail);
-           console.log(detail); 
+           //console.log(detail); 
         });
 
-        //console.log ('Messages ---');
         details.forEach(function (detail) {
            var message = builder.buildMessage(detail);
            //console.log(message); 
         });
-        
 
         expect(typeof builder).toBe('object');
-        
     });
-    
-    xit('should build Message', function(){ });
-    
+
+    it('should build Message', function(){ 
+
+        var reader = new Midi.Reader();
+        var input = reader.read(_buffer);
+        var builder = new Midi.Builder();
+
+        var detail = builder.buildMidiDetails(input);
+        //console.log(detail);
+        
+        expect(detail.tracks.length).toBe(1);
+
+        var messages = builder.buildMidiMessages(detail);
+        //console.log(messages);
+    });
+
 });
--- src/midi.builder.js
+++ src/midi.builder.js
@@ -1,24 +1,76 @@
 window.Midi = window.Midi || {};
 
 window.Midi.Builder = function (){
-                     
+
     return {
         buildDetail: buildDetail,
-        buildMessage: buildMessage
+        buildMessage: buildMessage,
+        buildMidiDetails: buildMidiDetails,
+        buildMidiMessages: buildMidiMessages
     };
-        
-    function buildDetail (message) {                        
-        
+
+    function buildMidiDetails (input) {
+
+        var builder = new Midi.Builder();
+
+        var output = { 
+            header: { 
+                type: input.header.type, 
+                timeDivision: input.header.timeDivision, 
+                trackCount: input.header.trackCount 
+            }, 
+            tracks: [] 
+        };
+
+        input.tracks.forEach(function (trkIn) {
+            var trkOut = [];
+            trkIn.forEach(function (msgIn) {
+                var msgOut = builder.buildDetail(msgIn);                
+                if (msgOut) trkOut.push(msgOut);
+            });
+            output.tracks.push(trkOut);
+        });
+
+        return output;
+    }
+
+    function buildMidiMessages (input) {
+
+        var builder = new Midi.Builder();
+
+        var output = { 
+            header: { 
+                type: input.header.type, 
+                timeDivision: input.header.timeDivision, 
+                trackCount: input.header.trackCount 
+            },
+            tracks: [] 
+        };
+
+        input.tracks.forEach(function (trkIn) {
+            var trkOut = [];
+            trkIn.forEach(function (msgIn) {
+                var msgOut = builder.buildMessage(msgIn);
+                if (msgOut) trkOut.push(msgOut);
+            });
+            output.tracks.push(trkOut);
+        });
+
+        return output;
+    }
+
+    function buildDetail (message) {
+
         var detail = {};
         var reader = (message.data) ? new BufferReader(message.data) : null;                                
-        
+
         switch (message.type) {
-                        
+
             // meta messages
             case 0x00:
-                detail.type = 'sequence-number';                
+                detail.type = 'sequence-number';
                 detail.number = reader.readInt16();
-                break;                
+                break;
             case 0x01:
                 detail.type = 'text';
                 detail.text = reader.readString(message.length);
@@ -46,20 +98,20 @@ window.Midi.Builder = function (){
             case 0x07:
                 detail.type = 'cue-point';
                 detail.text = reader.readString(message.length);
-                break;                
+                break;
             case 0x20:
-                detail.type = 'midi-channel-prefix';                
+                detail.type = 'midi-channel-prefix';
                 detail.channel = reader.readInt8();
-                break;                
+                break;
             case 0x2f:
-                detail.type = 'end-of-track';                
-                break;                
+                detail.type = 'end-of-track';
+                break;
             case 0x51:
-                detail.type = 'set-tempo';                
+                detail.type = 'set-tempo';
                 detail.microsecondsPerBeat = ((reader.readUint8() << 16) + (reader.readUint8() << 8) + reader.readUint8());
                 break;
             case 0x54:
-                detail.type = 'smpte-offset';                
+                detail.type = 'smpte-offset';
                 var hourByte = reader.readInt8();
                 detail.frameRate = { 0x00: 24, 0x20: 25, 0x40: 29, 0x60: 30 }[hourByte & 0x60];
                 detail.hour = hourByte & 0x1f;
@@ -69,14 +121,14 @@ window.Midi.Builder = function (){
                 detail.subframe = reader.readInt8();
                 break;
             case 0x58:
-                detail.type = 'time-signature';                
+                detail.type = 'time-signature';
                 detail.numerator = reader.readInt8();
                 detail.denominator = Math.pow(2, reader.readInt8());
                 detail.metronome = reader.readInt8();
                 detail.thirtyseconds = reader.readInt8();
                 break;
             case 0x59:
-                detail.type = 'key-signature';                
+                detail.type = 'key-signature';
                 detail.key = reader.readInt8();
                 detail.scale = reader.readInt8();
                 break;
@@ -85,7 +137,7 @@ window.Midi.Builder = function (){
                 detail.data = reader.read(length);
                 break;
 
-            // channel messages         
+            // channel messages
             case 0x08:
                 detail.type = 'note-off';
                 detail.note = message.param1;
@@ -94,7 +146,7 @@ window.Midi.Builder = function (){
             case 0x09:
                 detail.type = (message.param2 === 0) ? 'note-off' : 'note-on';
                 detail.note = message.param1;
-                detail.velocity = message.param2;                					
+                detail.velocity = message.param2;
                 break;
             case 0x0a:
                 detail.type = 'note-aftertouch';
@@ -118,39 +170,38 @@ window.Midi.Builder = function (){
                 detail.type = 'pitch-bend';
                 detail.value = message.param1 + (message.param2 << 7);
                 break;
-                                                
-            default:                
+
+            default:
                 console.log('Unrecognised Message Type: ' + message.type);          
         }
 
         if (message.data === undefined) {
             detail.channel = message.channel || 0;
-            detail.time = message.time || 0;            
+            detail.time = message.time || 0;
         }
-        
-        return detail;        
+
+        return detail;
     }
-        
-        
+
     function buildMessage (detail) {
-                
+
         var writer = new BufferWriter();
         var message = {};
-        
+
         if (detail.channel) message.channel = detail.channel;
         if (detail.time) message.time = detail.time;
-    
+
         switch (detail.type) {
 
             // meta messages
             case 'sequence-number':
                 message = buildLongMessage(0x00, writer.readInt16(detail.number).getBuffer());
-                break;  
+                break;
             case 'text':
                 message.type = 0x01;
                 message.data = writer.writeString(detail.text).getBuffer();
                 message.length = message.data.byteLength;
-                break;                
+                break;
             case 'copyright':
                 message.type = 0x02;
                 message.data = writer.writeString(detail.text).getBuffer();
@@ -184,61 +235,61 @@ window.Midi.Builder = function (){
             case 'midi-channel-prefix':
                 message.type = 0x20;
                 message.data = writer.writeInt8(detail.channel).getBuffer();
-                message.length = message.data.byteLength;                                                
-                break;                
+                message.length = message.data.byteLength;
+                break;
             case 'end-of-track':
                 message.type = 0x2f;
                 message.data = null;
-                message.length = 0;                
+                message.length = 0;
                 break;                
             case 'set-tempo':
                 message.type = 0x51;
-                var hex = detail.microsecondsPerBeat.toString(16);                
-                writer.writeUint8(parseInt(hex.substring(0, 1), 16));                                
+                var hex = detail.microsecondsPerBeat.toString(16);
+                writer.writeUint8(parseInt(hex.substring(0, 1), 16));
                 writer.writeUint8(parseInt(hex.substring(1, 3), 16));
                 writer.writeUint8(parseInt(hex.substring(3, 5), 16));
                 message.data = writer.getBuffer();
-                message.length = message.data.byteLength;                                            
+                message.length = message.data.byteLength;
                 break;
-                
-                
-            // channel messages          
+
+            // channel messages
             case 'note-off':
                 message.type = 0x08;
                 message.param1 = detail.note;
-                message.param2 = detail.velocity;                               
+                message.param2 = detail.velocity;
                 break;
             case 'note-on':
                 message.type = 0x09;
                 message.param1 = detail.note;
-                message.param2 = detail.velocity;                                                     					
+                message.param2 = detail.velocity;
                 break;
             case 'note-aftertouch':
                 message.type = 0x0a;
                 message.param1 = detail.note;
-                message.param2 = detail.amount;                
-                break;                
+                message.param2 = detail.amount;
+                break;
             case 'controller':
                 message.type = 0x0b;
                 message.param1 = detail.controllerType;
-                message.param2 = detail.value;                                                             
+                message.param2 = detail.value;
                 break;
             case 'program-change':
                 message.type = 0x0c;
                 message.param1 = detail.programNumber;
-                message.param2 = null;                           
+                message.param2 = null;
                 break;
             case 'channel-aftertouch':
                 message.type = 0x0c;
                 message.param1 = detail.amount;
-                message.param2 = null;                                        
-                break;                                               
+                message.param2 = null;
+                break;
                 
             default: 
-                console.log('Unrecognised Message Type: ' + detail.type);                 
+                console.log('Unrecognised Message Type: ' + detail.type);
+                message = null;
         }
-        
-        return message;               
+
+        return message;
     }
-           
+
 };
